736497a829337900bf182f2c8301276a479d4144
bepasar <bepasar@gmail.com>
2021-09-18 21:18:42 -0300

    modificação na modelagem da parsing table* ao invés de fazer um dicionário com um frozenset contendo o não terminal e o terminal como chave, troquei para ser um dicionário dos NT's cada um contendo um dicionário com os terminais que tiverem alguma entrada na tabela como chave fiz isso para facilitar a montagem da parsing table na interface gráfica

--- model/syntactic.py
+++ model/syntactic.py
@@ -6,25 +6,14 @@ class Syntactic():
         self.grammar =  grammar
         self.parsing_table = dict()
 
-    # inicializa a tabela de análise (dicionário)
-    # sendo cada entrada formada por um conjunto contendo
-    # um não terminal e um terminal (ou '$') da gramática
-    def init_parsing_table(self, grammar: Grammar):
-        parsing_table = dict()
-        for nt in grammar.nonterminal:
-            for t in grammar.terminal | {'$'}:
-                parsing_table[frozenset({nt, t})] = []
-
-        return parsing_table
-
     # método que aplica o algoritmo de gerar 
     # a tabela de análise mostrado em aula.
     def generate_parsing_table(self, grammar: Grammar):
-        parsing_table = self.init_parsing_table(grammar)
+        parsing_table = {nt: dict() for nt in grammar.nonterminal}
         for head, prods in grammar.productions.items():
             for p in prods:
                 if p[0] in grammar.terminal:
-                    parsing_table[ frozenset( {head, p[0]} ) ] = p
+                    parsing_table[head][p[0]] = p
 
                 elif p[0] in grammar.nonterminal:
                     length = len(p)
@@ -33,7 +22,7 @@ class Syntactic():
                         # incluir a produção na tabela para
                         # todo símbolo em first do NT (p[i])  
                         for a in grammar.first[ p[i] ] - {'&'}:
-                            parsing_table[ frozenset( {head, a} ) ] = p
+                            parsing_table[head][a] = p
 
                         # condição para não continuar verificando 
                         # os próximos símbolos da produção
@@ -45,11 +34,11 @@ class Syntactic():
                     # (caso toda a sequência da produção seja anulável)
                     if i + 1 == length:
                         for a in grammar.follow[head]:
-                            parsing_table[ frozenset( {head, a} ) ] = p
+                            parsing_table[head][a] = p
 
                 elif p[0] == '&':
                     for a in grammar.follow[head]:
-                        parsing_table[ frozenset( {head, a} ) ] = p
+                        parsing_table[head][a] = p
 
         self.parsing_table = parsing_table
 
@@ -82,7 +71,7 @@ class Syntactic():
             # eh nonterminal
             else:
                 try:
-                    parsing_table_list = self.parsing_table[frozenset({sentence_item, stack[-1]})]
+                    parsing_table_list = self.parsing_table[stack[-1]][sentence_item]
                     if parsing_table_list == []:
                         print('not accepted ([])')
                         break
